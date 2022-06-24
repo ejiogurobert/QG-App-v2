c67ade2e8b74ead6dd49b9edf9d8b75123032124
Robert Ejiogu <robertejiogu@gmail.com>
2022-06-24 12:37:06 +0100

    created a view folder and functionalities on the server index to access to other user end to be hoisted on the heroku

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,8 @@ const app = express();
 const host = process.env.HOST;
 const port = process.env.port || 3000;
 
+app.use(express.static('./views'))
+
 expressConfig(app);
 db.connect()
   .then((obj) => {
